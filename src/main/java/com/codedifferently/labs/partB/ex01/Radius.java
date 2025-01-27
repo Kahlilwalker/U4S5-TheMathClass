d300c5ccc932a7bd0f6b749b1c93dac96783621a
Kahlilwalker <Kahlilmyson@gmail.com>
2025-01-27 13:18:56 -0500

    work

--- src/main/java/com/codedifferently/labs/partB/ex01/Radius.java
+++ src/main/java/com/codedifferently/labs/partB/ex01/Radius.java
@@ -1,5 +1,10 @@
 package com.codedifferently.labs.partB.ex01;
 import java.util.Scanner;
 public class  Radius {
+    public static void main(String[] args){
+        double area = 314.16;
+        double answer = Math.sqrt(area / Math.PI);
+        System.out.println(answer);
 
+    }
 }
--- src/main/java/com/codedifferently/labs/partB/ex02/Maximum.java
+++ src/main/java/com/codedifferently/labs/partB/ex02/Maximum.java
@@ -1,4 +1,17 @@
 package com.codedifferently.labs.partB.ex02;
+import java.util.Random;
 
 public class Maximum {
+
+
+    public static void main(String[] args) {
+        int x = (int) (Math.random() * 10);
+        int y = (int) (Math.random() * 10);
+
+        // Find the maximum of x and y
+        int max = Math.max(x, y);
+
+        System.out.println("The maximum value is: " + max); // Output: 20
+    }
 }
+
